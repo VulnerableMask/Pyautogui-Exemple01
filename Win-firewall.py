6d4431d0e6be1c136c623e3b1595799ad9a3a278
VulnerableMask <101364621+VulnerableMask@users.noreply.github.com>
2022-03-10 16:06:32 -0500

    Add files via upload

---
+++ Win-firewall.py
@@ -0,0 +1,44 @@
+import pyautogui
+import time
+
+#Automação para desligar firewall do windows 
+
+pyautogui.hotkey('winlft', 'x')
+time.sleep(1)
+pytuatogui.
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down');
+pyautogui.press('down')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.press('left')
+time.sleep(1)
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.write('nesth advfirewall set allprofiles state off')
+time.sleep(1)
+#FIM
+
+pyautogui.hotkey('winlft')
+time.sleep(1)
+pytuatogui.write('powershell')
+time.sleep(1)
+pyautogui.press('right');
+time.sleep(1)
+pyautogui.press('down');
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.press('left');
+pyautogui.press('enter')
+time.sleep(2)
+pyautogui.write('nesth advfirewall set allprofiles state off')
+time.sleep(1)
